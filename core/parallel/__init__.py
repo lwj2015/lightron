ea98abbb1c6fa0aad4b702acf761bcda7516a983
liuwenjun.sspku <liuwenjun.sspku@bytedance.com>
2025-12-18 23:23:20 +0800

    improve parallel_init

--- core/parallel/__init__.py
+++ core/parallel/__init__.py
@@ -1,3 +1,19 @@
+# lightron/parallel/__init__.py
+
 from .distributed import setup_distributed, get_device_mesh
 from .parallel_fsdp import apply_fsdp1, apply_fsdp2
 from .parallel_tp import ColumnParallelLinear, RowParallelLinear, VocabParallelEmbedding
+from .parallel_cp import ContextParallelAttention
+from .parallel_ep import ExpertParallel
+from .parallel_pp import PipelineStage
+from .parallel_dp import DataParallel
+
+__all__ = [
+    "setup_distributed", "get_device_mesh",
+    "apply_fsdp1", "apply_fsdp2",
+    "ColumnParallelLinear", "RowParallelLinear", "VocabParallelEmbedding",
+    "ContextParallelAttention",
+    "ExpertParallel",
+    "PipelineStage",
+    "DataParallel"
+]
